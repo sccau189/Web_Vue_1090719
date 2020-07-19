fd39be1f7a638f44ffe7d5aaff9e2761b4bad7bf
sccau189 <sccau189@hotmail.com>
2020-07-19 16:03:12 +0800

    vue 結束

--- main.js
+++ main.js
@@ -26,5 +26,52 @@ var vue3 = new Vue({
     data:{
         // 布林值
         text: true,
+        // 當 true時 顯示 v-if判斷式的內容，當 false 時 顯示v-else 判斷式的內容
+        activity:true,
+    }
+})
+
+var vue4 = new Vue({
+    el:"#vue4",
+    data:{
+        products:[
+            {name:"有機優格", price: 60, text:"採無添加物"},
+            {name:"水果優格", price: 70, text:"採當天現採"},
+            {name:"巧克力優格", price: 55, text:"採70%加物"},
+            {name:"五穀優格", price: 65, text:"採清爽無負擔"},
+            {name:"客製化優格", price: '60+up' , text:"隨心添加"},
+        ]
+    }
+})
+
+var vue5 = new Vue({
+    el:"#vue5",
+    data :{
+        link:"",
+        text:"",
+        count:0,
+        pic:""
+    },
+    methods: {
+        // show為自定義 隨意命名
+        show: function() {
+            this.link ="https://tigersugar.com/"
+        },
+        showon: function() {
+            this.text = "點到了"
+        },
+        add: function(){
+            this.count++;
+        },
+        sub: function(){
+            this.count--;
+        },
+    }
+});
+
+var vue6 = new Vue({
+    el: "#vue6",
+    data: {
+        name:""
     }
 })
